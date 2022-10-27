6e4fd1bbcd2e98cd428159ba454dde49afdd51bb
Sam Clements <sam@borntyping.co.uk>
2022-10-27 11:02:41 +0100

    Explicitly list imports

--- riemann_client/__init__.py
+++ riemann_client/__init__.py
@@ -1,19 +1,31 @@
 """A Python Riemann client and command line tool"""
 
-from .client import (
-    Client, QueuedClient  # noqa
-)
-
+from .client import Client, QueuedClient
 from .transport import (
-    RiemannError, SocketTransport, UDPTransport,  # noqa
-    TCPTransport, TLSTransport, BlankTransport,   # noqa
+    BlankTransport,
+    RiemannError,
+    SocketTransport,
+    TCPTransport,
+    TLSTransport,
+    UDPTransport,
 )
-
 
 try:
-    from .client import AutoFlushingQueuedClient  # noqa
+    from .client import AutoFlushingQueuedClient
 except ImportError:
     pass
 
 __version__ = '6.4.0'
 __author__ = 'Sam Clements <sam.clements@datasift.com>'
+
+__all__ = (
+    "AutoFlushingQueuedClient",
+    "BlankTransport",
+    "Client",
+    "QueuedClient",
+    "RiemannError",
+    "SocketTransport",
+    "TCPTransport",
+    "TLSTransport",
+    "UDPTransport",
+)
